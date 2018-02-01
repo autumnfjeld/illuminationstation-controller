09aa3ce31b89e830bdb5d04434529555cdc42066
Autumn Fjeld <autumn@ustwo.com>
2018-02-01 17:10:27 +1100

    Working with proxy and DF

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 'use strict';
-const express = require('express')
-const util = require('util')
-const bodyParser = require('body-parser')
-const app = express()
-const partyLights = require('./hue.js')
+const express = require('express');
+const util = require('util');
+const bodyParser = require('body-parser');
+const app = express();
+const lightStates = require('./hue.js');
+
 
 const jsonParser = bodyParser.json({
     extended: true
@@ -13,45 +14,104 @@ const jsonParser = bodyParser.json({
 * Logging
 */
 const logStuff = function(req, res, next) {
-    // console.log('\n __req__ \n: ', req)
-    console.log('\nRequest Header__ \n ', req.headers)
-    // console.log('\n __Request \n ', req);
+    console.log('\no0o0oo00o0O0o0o0O0o0 app lOgGinG o0o0oo00o0O0o0o0O0o0');
+    console.log(' * request headers \n ', req.headers);
+    // console.log('\njsonParser req.body\n', req.body);
+    // console.log('\njsonParser req.body.responseId\n', req.body.responseId);
+    // console.log('\njsonParser req.body.queryResult\n', req.body.queryResult);
     // console.log('\n __Request originalURL__ \n ', req.originalUrl);
-    console.log('\nRequest Body (jsonParser)__ \n ', req.body)
+    // console.log('\nRequest Body (jsonParser)__ \n ', req.body)
     // console.log('\n __Request Params__ \n ', req.params);
     // console.log('\n __Request Query__ \n ', req.query);
-    next()
+    next();
+}
+
+const logDialogFlowPost = function(req) {
+    console.log('\no0o0oo00o0O0o0o0O0o0  Dialogflow lOgGinG o0o0oo00o0O0o0o0O0o0')
+    console.log(' *  req.body.responseId\n', req.body.responseId);
+    console.log(' *  req.body.queryResult\n', req.body.queryResult);
+}
+
+const logLightResponse = (result) => {
+    console.log('\nLight state changed:', result);
 }
 
 app.use(logStuff)
 
+app.get('/', (req, res) => res.send({message:'Hi! You\'ve reached the Illumination Station'}));
+
+app.get('/healthcheck', jsonParser, (req, res) => {
+
+    lightStates.setState('party');
+    setTimeout( () => lightStates.setState('oops'),  5000);
+    setTimeout(() => lightStates.setState('soothing'), 8000);
+    setTimeout(() => lightStates.setState('purple'), 10000);
+    setTimeout(() => {
+        res.send({message: 'Healthcheck done! You shoulda seen a hellavalotta lights'})
+    }, 11000);
+})
+
+// app.get('/getstate', jsonParser, (req, res) => {
+//
+// })
+
 app.post('/', jsonParser, (req, res) => {
-    if (!req.body || !Object.keys(req.body)) return res.sendStatus(400)
-    console.log('jsonParser body', req.body);
-
-    let responseToProxy = {
-        statusCode: 200,
-        body: {
-            message:'Got a POST: You\'ve reached the Illumination Station!',
-            source: 'IlluminationStation studio app',
-            fulfillmentText: 'Horray! changing the lights for you!',
-        }
+    if (!req.body || !Object.keys(req.body)) {
+        return res.sendStatus(400)
     }
+    logDialogFlowPost(req);
+    // TODO handle other response erros
+    res.status = 200
+    var resBody = {
+        source: 'IlluminationStation studio app',
+        fulfillmentText: "sdsd"
+    };
+    // let responseToProxy = {
+    //     statusCode: 200,
+    //     body: {
+    //         message:'Got a POST: You\'ve reached the Illumination Station!',
+    //         source: 'IlluminationStation studio app',
+    //         fulfillmentText: 'Horray! changing the lights for you!',
+    //     }
+    // }
 
-    partyLights()
-        .then( (result) => console.log('\nHue Light State:', JSON.stringify(result, null, 2)))
-        .done(res.send(responseToProxy))
-    // res.send(responseToProxy)
+    let msgs = req.body.queryResult.fulfillmentMessages
+    let customPayload = msgs.find( (msg) => msg.hasOwnProperty('payload') )
+    const lightResponse = customPayload.payload.lightresponse;
+    console.log('Found custom payload', customPayload);
+    console.log('lightresponse:', customPayload.payload.lightresponse)
+
+    switch (lightResponse) {
+        case 'partylights':
+            resBody.fulfillmentText = 'I\'m puttin on the disco lights! Let\'s boogie!'
+            lightStates.setState('party')
+                .then(logLightResponse)
+                .done(res.json(resBody))
+            break
+        case 'soothing':
+            resBody.fulfillmentText = 'I feel you.  Why don\'t you get cosy on the couch and I\'ll set the ambiance for you.  Do you need a massage?'
+            lightStates.setState('soothing')
+                .then(logLightResponse)
+                .done(res.json(resBody))
+            break
+        default:
+            console.log('Oops! should not get here:', lightresponse)
+            resBody.body.fulfillmentText = 'Oops. I\'m in the dark with this one. But maybe this will light you up.'
+            lightStates.setState('oops')
+                .then(logLightResponse)
+                .done(res.json(resBody))
+
+    }
 })
 
 
-app.put('/', (req, res) => res.send({message:'Got a PUT: You\'ve reached the Illumination Station!'}))
+app.put('/', (req, res) => res.send({message:'There is nothing to PUT in the Illumination Station'}))
 
-// app.get('/healthcheck', (req, res) => res.sendStatus(200))
-//
-// app.get('/partylights', (req, res) => {
-//     res.send({message: 'Party Lights!!'})
-// })
+    // app.get('/healthcheck', (req, res) => res.sendStatus(200))
+    //
+    // app.get('/partylights', (req, res) => {
+    //     res.send({message: 'Party Lights!!'})
+    // })
 
 
 app.listen(9800, () => console.log('Example app listening on port 9800!'))
--- hue.js
+++ hue.js
@@ -1,46 +1,152 @@
-const hue = require("node-hue-api")
+const hue = require("node-hue-api");
 
+/****** Logging *******/
 function displayBridges(bridge) {
-	console.log("Hue Bridges Found: " + JSON.stringify(bridge))
+	console.log("Hue Bridges Found: " + JSON.stringify(bridge));
 }
+function displayResult(description, result) {
+	console.log('\n',description, '\n', JSON.stringify(result, null, 2), '\n');
+}
+function logSetState(stateLabel, stateValues) {
+	console.log('\no0o0oo00o00o0o0O0o00oO0o0o0O0o00oo00Oo\n' +
+		'Setting ', stateLabel, 'light state with values:\n' , stateValues ,
+	  	'\no0o0oo00o0O0o0o0O0o00oO0o000o00oo0o0Oo');
+}
+// function displayLightState(result) {
+//     console.log('\nHue LIGHT STATE:', JSON.stringify(result, null, 2))
+// }
+/******* Find and configure hue bridge *****/
 
-hue.nupnpSearch().then(displayBridges).done()
-
-// --------------------------
-// Using a callback
-// hue.nupnpSearch(function(err, result) {
-// 	if (err) throw err;
-// 	displayBridges(result);
-// });
+// Find hue bridge
+hue.nupnpSearch().then(displayBridges).done();
 
 // Credentials
 const host = '10.4.0.54'
 const username = 'cq5Yb7Me6tLAS-hxKUDsm9oRtUKpGG24edGUsO01'
 const api = new hue.HueApi(host, username)
 
-// Verify
-function displayResult(result) {
-    console.log('\nHue RESULT:', JSON.stringify(result.lights, null, 2))
-}
-
-function displayLightState(result) {
-    console.log('\nHue LIGHT STATE:', JSON.stringify(result, null, 2))
-}
-
 api.config()
-    .then(displayResult)
+    .then((result) => displayResult('CONFIG', result))
     .done();
 
+api.setLightState
+let initState = hue.lightState.create().on();
+
 api.lights()
-    .then(displayResult)
+    .then( (result) => {
+		console.log('Reseting light bulb ')
+		displayResult('EXISTING STATE', result)
+		initState.reset();
+		// api.setLightState(1, {});
+		// api.setLightState(1, initState.reset())
+		// 	.then( (result) => {
+		// 		logSetState('RESET. NEW STATE', result)
+		// 	});
+		// api.setLightState(1, state.reset())
+		// api.setLightState(1, state);
+	})
     .done()
 
-const lightState = hue.lightState
+const resetState = {
+	"on": true,
+	"bri": 254,
+		"hue": 10,
+		"sat": 254,
+		"effect": "none",
+		"xy": [
+			0.4288,
+			0.5034
+		],
+		"ct": 319,
+		"alert": "none"
+};
 
-function makePartyLights() {
-	// Create a new lightStat objerct to pass to a Philips Hue Light
-	let partyState = lightState.create().on().transitionFast().colorLoop()
-	return api.setLightState(1, partyState)
+initState.reset();
+// api.setLightState(1, initState.reset())
+api.setLightState(1, resetState)
+	.then( (result) => {
+		console.log("WTHD", result);
+		logSetState('initState.reset', initState._values)
+		api.lights().then( (result) => displayResult('RESET', result));
+	});
+
+// const lightState = hue.lightState
+// Clear all light states
+// api.setLightState(1, hue.lightState.create().off())
+
+const lightStates = {
+
+	currentState: initState,
+
+	party: hue.lightState.create().on().transitionFast().xy(.25,.11).colorLoop().alert('lselect'),
+
+	soothing: hue.lightState.create().on().transitionInstant().effect('none').xy(0.57, 0.34),
+
+	oops: hue.lightState.create().on().transitionFast().alert('select').xy(0.08, 0.61),
+
+	purple: hue.lightState.create().on().transitionFast().xy(0.24, 0.08),
+
+	setState: function (state){
+		// this.resetState()
+		this.currentState = this[state]
+		logSetState(state, this[state]._values);
+		return api.setLightState(1, this[state]);
+
+		// Clear old state
+		// api.setLightState(1, this.currentState.off())
+		// 	.then( () => {
+		// 		this.currentState = this[state]
+		// 		logSetState(state, this[state]._values);
+		// 		api.lights()
+		// 		    .then( (result) => {
+		// 				displayResult('EXISTING STATE', result);
+		// 			})
+		// 			.then( () => {
+		// 				return api.setLightState(1, this[state]);
+		// 			})
+        //
+		// 	});
+	},
+
+	// resetState: function(){
+	// 	api.setLightState(1, resetState)
+	// 		.then( () => {displayResult()});
+	// },
+
+
+
+	off: function () {
+		return api.setLightState(1, this.currentState.off()).then(logSetState('off', this.state));
+
+	},
+
+	// party: function () {
+	// 	// Create a new lightStat objerct to pass to a Philips Hue Light
+	// 	this.state.reset()
+	// 	// this.state = hue.lightState.create().on().transitionFast().colorLoop()
+	// 	logSetState('party', this.state)
+	// 	return api.setLightState(1, this.state.transitionFast().colorLoop())
+	// },
+    //
+	// soothing: function () {
+	// 	console.log('this', this);
+	// 	// this.state = null
+	// 	// this.state = hue.lightState.create().on().transitionInstant().xy(0.57, 0.34)
+	// 	this.state.reset()
+	// 	logSetState('soothing', this.state)
+	// 	return api.setLightState(1, this.state.reset().transitionInstant().xy(0.57, 0.34))
+	// },
+    //
+	// oops: function () {
+	// 	// this.state = null
+	// 	// this.state = hue.lightState.create().on().transitionFast().alert('lselect').xy(0.08, 0.61)
+	// 	console.log('this.state', this.state);
+	// 	this.state.transitionFast().alert('lselect').xy(0.08, 0.61)
+	// 	logSetState('oops', this.state)
+	// 	return api.setLightState(1, this.state)
+	// }
 }
+// console.log('lightStates', lightStates);
 
-module.exports = makePartyLights
+
+module.exports = lightStates
